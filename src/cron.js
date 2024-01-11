ff8b8881c0bc4a01c70167ccd3cfe9d24227424f
Jason <jason.dion.taff@gmail.com>
2024-01-11 16:08:46 -0500

    config updates and cron.js updates

--- src/cron.js
+++ src/cron.js
@@ -1,11 +1,24 @@
 const cron = require('node-cron');
 const { spawn } = require('child_process');
+const fs = require('fs');
+
+// Lock file path
+const lockFilePath = 'cron.lock';
 
 // Define the schedule for the cron job (7 AM, noon, and 4 PM Monday through Friday, and midnight every day)
 const schedule = '0 7,12,16 * * 1-5,0';
 
 // Create the cron job
 cron.schedule(schedule, () => {
+  // Check if the lock file exists
+  if (fs.existsSync(lockFilePath)) {
+    console.log('Previous job is still running. Skipping the current scheduled run.');
+    return;
+  }
+
+  // Create the lock file
+  fs.writeFileSync(lockFilePath, '');
+
   // Define the command to run sync.js
   const command = 'sync.js';
 
@@ -24,6 +37,9 @@ cron.schedule(schedule, () => {
 
   // Handle the exit of the child process
   child.on('close', (code) => {
+    // Remove the lock file after the job finishes
+    fs.unlinkSync(lockFilePath);
+
     if (code === 0) {
       console.log('sync.js executed successfully.');
     } else {
